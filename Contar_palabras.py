38993d6d22a3e656306d3d3b9a56bd8e108e377c
reynier124 <reynier1306@gmail.com>
2023-04-11 19:01:07 -0300

    contar palabras y letras

--- Contar_palabras.py
+++ Contar_palabras.py
@@ -1,9 +1,14 @@
 import unittest
 
 def contar_palabras(oracion): 
-    cantidad = len(oracion.split(" "))
-    palabras = {oracion : cantidad}
-    return palabras
+    resultado = {}
+    for word in oracion.split(' '):
+        if word in resultado:
+            resultado[word.lower()] += 1
+        else:
+            resultado[word.lower()] = 1
+    return resultado
+        
 
 if __name__ == "__main__":
     unittest.main()
